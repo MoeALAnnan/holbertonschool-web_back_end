cde7f5536e02aaf665910c37d8ba3e4cab7ec84a
MoeAlAnnan <5653@holbertonstudents.com>
2024-01-13 13:33:24 +0000

    8-all.py

---
+++ NoSQL/8-all.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+    Module
+"""
+
+import pymongo
+
+
+def list_all(mongo_collection):
+    """
+    Function comment:
+    This function takes a MongoDB collection and key-value pairs (**kwargs) as input,
+    """
+    return mongo_collection
