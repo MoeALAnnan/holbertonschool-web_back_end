7fd19f9d2683cd9c16201bf4dc4e89c553cc0519
MoeAlAnnan <5653@holbertonstudents.com>
2024-01-19 17:34:19 +0000

    5-building.js

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,13 +1,15 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor !== Building) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
   }
+
   get sqft() {
     return this._sqft;
   }
+
   set sqft(newSqft) {
     this._sqft = newSqft;
   }
