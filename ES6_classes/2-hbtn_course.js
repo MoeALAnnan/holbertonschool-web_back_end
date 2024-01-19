7e6cd94e411b134b62512178e1d48c0ec9a1748e
MoeAlAnnan <5653@holbertonstudents.com>
2024-01-19 17:30:27 +0000

    2-hbtn_course.js

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,44 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = HolbertonCourse.validateString(name, 'Name');
+    this._length = HolbertonCourse.validateNumber(lenght, 'Length');
+    this._students = HolbertonCourse.validateArray(students, 'Students');
+  }
+
+  get name() {
+    return this._name;
+  }
+  set name(newName) {
+    this._name = HolbertonCourse.validateString(newName, 'Name');
+  }
+  get length() {
+    return this._length;
+  }
+  set length(newLength) {
+    this._length = HolbertonCourse.validateNumber(newLength, 'Length');
+  }
+  get students() {
+    return this._students;
+  }
+  set students(newStudents) {
+    this._students = HolbertonCourse.validateArray(newStudents, 'Students');
+  }
+  static validateString(value, attribute) {
+    if (typeof value !== 'string') {
+      throw new TypeError(`${attribute} must be a string`);
+    }
+    return value;
+  }
+  static validateNumber(value, attribute) {
+    if (typeof value !== 'number') {
+      throw new TypeError(`${attribute} must be a number`);
+    }
+    return value;
+  }
+  static validateArray(value, attribute) {
+    if (!Array.isArray(value)) {
+      throw new TypeError(`${attribute} must be an array of strings`);
+    }
+    return value;
+  }
+}
